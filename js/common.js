7e0e63684d7b10e4ec62551c761de3802c1e1692
kike300166 <131197654+kike300166@users.noreply.github.com>
2023-05-14 00:44:49 -0400

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,34 @@
+/*
+
+  ____          _____               _ _           _       
+ |  _ \        |  __ \             (_) |         | |      
+ | |_) |_   _  | |__) |_ _ _ __ _____| |__  _   _| |_ ___ 
+ |  _ <| | | | |  ___/ _` | '__|_  / | '_ \| | | | __/ _ \
+ | |_) | |_| | | |  | (_| | |   / /| | |_) | |_| | ||  __/
+ |____/ \__, | |_|   \__,_|_|  /___|_|_.__/ \__, |\__\___|
+         __/ |                               __/ |        
+        |___/                               |___/         
+    
+____________________________________
+/ Si necesitas ayuda, contáctame en \
+\ https://parzibyte.me               /
+ ------------------------------------
+        \   ^__^
+         \  (oo)\_______
+            (__)\       )\/\
+                ||----w |
+                ||     ||
+Creado por Parzibyte (https://parzibyte.me). Este encabezado debe mantenerse intacto,
+excepto si este es un proyecto de un estudiante.
+*/
+// Tomado de https://parzibyte.me/blog/2019/06/26/menu-responsivo-bootstrap-4-sin-dependencias/
+document.addEventListener("DOMContentLoaded", () => {
+    const menu = document.querySelector("#menu"),
+        botonMenu = document.querySelector("#botonMenu");
+    if (menu) {
+        botonMenu.addEventListener("click", () => menu.classList.toggle("show"));
+    }
+});
+const LOCAL_STORAGE_WORDS_KEY = "words";
+const getWords = () => (JSON.parse(localStorage.getItem(LOCAL_STORAGE_WORDS_KEY)) || []);
+const saveWords = (words) => (localStorage.setItem(LOCAL_STORAGE_WORDS_KEY, JSON.stringify(words)));
---
+++ js/game.js
@@ -0,0 +1,151 @@
+/*
+
+  ____          _____               _ _           _       
+ |  _ \        |  __ \             (_) |         | |      
+ | |_) |_   _  | |__) |_ _ _ __ _____| |__  _   _| |_ ___ 
+ |  _ <| | | | |  ___/ _` | '__|_  / | '_ \| | | | __/ _ \
+ | |_) | |_| | | |  | (_| | |   / /| | |_) | |_| | ||  __/
+ |____/ \__, | |_|   \__,_|_|  /___|_|_.__/ \__, |\__\___|
+         __/ |                               __/ |        
+        |___/                               |___/         
+    
+____________________________________
+/ Si necesitas ayuda, contáctame en \
+\ https://parzibyte.me               /
+ ------------------------------------
+        \   ^__^
+         \  (oo)\_______
+            (__)\       )\/\
+                ||----w |
+                ||     ||
+Creado por Parzibyte (https://parzibyte.me). Este encabezado debe mantenerse intacto,
+excepto si este es un proyecto de un estudiante.
+*/
+const ALPHABET = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ",
+    MAX_ATTEMPTS = 6,
+    MASK_CHAR = "_";
+new Vue({
+    el: "#app",
+    data: () => ({
+        letters: {},
+        hiddenWord: [],
+        remainingAttempts: 0,
+    }),
+    async mounted() {
+        await Swal.fire(
+            'Hangman game',
+            'Proudly brought to you by parzibyte - https://parzibyte.me',
+            'info'
+        );
+        this.resetGame();
+    },
+    methods: {
+        resetGame() {
+            this.resetAttempts();
+            this.setupKeys();
+            this.chooseWord();
+        },
+        checkGameStatus() {
+            if (this.playerWins()) {
+                Swal.fire("You win! The word was " + this.getUnhiddenWord());
+                this.resetGame();
+            }
+            if (this.playerLoses()) {
+                Swal.fire("You lose. The word was " + this.getUnhiddenWord());
+                this.resetGame();
+            }
+        },
+        getUnhiddenWord() {
+            let word = "";
+            for (const letter of this.hiddenWord) {
+                word += letter.letter;
+            }
+            return word;
+        },
+        playerWins() {
+            // If there's at least a hidden letter, the player hasn't win yet
+            for (const letter of this.hiddenWord) {
+                if (letter.hidden) {
+                    return false;
+                }
+            }
+            return true;
+        },
+        playerLoses() {
+            return this.remainingAttempts <= 0;
+        },
+        imagePath() {
+            return `img/Ahorcado-${MAX_ATTEMPTS - this.remainingAttempts}.png`;
+        },
+        resetAttempts() {
+            this.remainingAttempts = MAX_ATTEMPTS;
+        },
+        async chooseWord() {
+            // Get words stored in localstorage
+            const words = getWords();
+            if (!words.length) {
+                await Swal.fire("Please add some words so you can play (go to Manage words)");
+                window.location = "./words.html";
+            }
+            // Choose random
+            let word = words[Math.floor(Math.random() * words.length)];
+            this.prepareWord(word);
+        },
+        prepareWord(word) {
+            word = word.toUpperCase();
+            const hiddenWord = [];
+            for (const letter of word) {
+                hiddenWord.push({
+                    letter,
+                    hidden: true,
+                });
+            }
+            this.hiddenWord = hiddenWord;
+        },
+        displayWord() {
+            let displayedWord = "";
+            for (const letter of this.hiddenWord) {
+                if (letter.hidden) {
+                    displayedWord += MASK_CHAR;
+                } else {
+                    displayedWord += letter.letter;
+                }
+                displayedWord += " ";
+            }
+            return displayedWord;
+        },
+        setupKeys() {
+            // We make a dictionary from the letters
+            for (const letter of ALPHABET) {
+                Vue.set(this.letters, letter, {
+                    letter,
+                    disabled: false, // We disable it when the user clicks on it
+                });
+            }
+        },
+        letterExistsInWord(searchedLetter) {
+            for (const letter of this.hiddenWord) {
+                if (letter.letter === searchedLetter) {
+                    return true;
+                }
+            }
+            return false;
+        },
+        discoverLetter(letter) {
+            for (const index in this.hiddenWord) {
+                if (this.hiddenWord[index].letter === letter) {
+                    this.hiddenWord[index].hidden = false;
+                }
+            }
+        },
+        attemptWithLetter(letter) {
+            Vue.set(this.letters[letter], "disabled", true);
+            if (!this.letterExistsInWord(letter)) {
+                this.remainingAttempts -= 1;
+            } else {
+                this.discoverLetter(letter);
+            }
+            this.checkGameStatus();
+        }
+    }
+});
